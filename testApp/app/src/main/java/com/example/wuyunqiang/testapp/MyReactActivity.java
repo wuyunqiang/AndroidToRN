ae37492533c923cd541d6e286934ed72b36349dd
wuyunqiang <2801213940@qq.com>
2017-12-01 16:26:27 +0800

    修改调试闪退bug 添加权限

--- testApp/app/src/main/java/com/example/wuyunqiang/testapp/MyReactActivity.java
+++ testApp/app/src/main/java/com/example/wuyunqiang/testapp/MyReactActivity.java
@@ -1,23 +1,38 @@
 package com.example.wuyunqiang.testapp;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Toast;
 
+import com.facebook.common.logging.FLog;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.common.LifecycleState;
+import com.facebook.react.common.ReactConstants;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.shell.MainReactPackage;
 
+import static android.os.Build.VERSION.SDK_INT;
+import static android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION;
+import static java.security.AccessController.getContext;
+
 public class MyReactActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler{
 
+    public static int OVERLAY_PERMISSION_REQ_CODE = 1234;
     static String TAG = "MyReactActivity";
     private ReactRootView mReactRootView;
     private ReactInstanceManager mReactInstanceManager;
+    Intent floatWinIntent;
     String name;
 
     @Override
@@ -25,21 +40,46 @@ public class MyReactActivity extends AppCompatActivity implements DefaultHardwar
         super.onCreate(savedInstanceState);
         name = getIntent().getStringExtra("name");
         Log.i(TAG,name);
+        this.askForPermission();
         Toast.makeText(this,name,Toast.LENGTH_LONG).show();
         mReactRootView = new ReactRootView(this);
         mReactInstanceManager = ReactInstanceManager.builder()
                 .setApplication(getApplication())
                 .setBundleAssetName("index.bundle")
-                .setJSMainModulePath("index")//这里版本发生变化了
+                .setJSMainModulePath("app")//这里版本发生变化了
                 .addPackage(new MainReactPackage())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
                 .setInitialLifecycleState(LifecycleState.RESUMED)
                 .build();
-
         // 注意这里的HelloWorld必须对应“index.android.js”中的
         // “AppRegistry.registerComponent()”的第一个参数
-        mReactRootView.startReactApplication(mReactInstanceManager, "testApp", null);
+//        mReactRootView.startReactApplication(mReactInstanceManager, "testApp", null);
+        mReactRootView.startReactApplication(mReactInstanceManager, "App", null);
         setContentView(mReactRootView);
+    }
+
+    @SuppressLint("NewApi")
+    public void askForPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "当前无权限，请授权！", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "权限授予失败，无法开启悬浮窗", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "权限授予成功！", Toast.LENGTH_SHORT).show();
+                //启动FxService
+            }
+
+        }
     }
 
     @Override
